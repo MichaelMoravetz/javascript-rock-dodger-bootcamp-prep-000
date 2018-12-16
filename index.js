b1831daf1367310b0e46529cf568b0b61e598e0a
Michael Moravetz <moravemv@gmail.com>
2018-12-16 19:41:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function createRock(x) {
      /* If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if (checkCollision() === true) {
+     if (checkCollision(rock) === true) {
        endGame();
      }
       
