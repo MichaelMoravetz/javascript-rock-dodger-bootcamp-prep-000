834ec3b05268b31e306c311e70fdb1c81ec3b4f4
Michael Moravetz <moravemv@gmail.com>
2018-12-16 19:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -127,8 +127,8 @@ function moveDodger(e) {
     moveDodgerLeft();
   }
   else if (e.which === RIGHT_ARROW) {
+    e.stopPropagation()
     e.preventDefault();
-    e.stopPropagation();
     moveDodgerRight();
   }
 }
